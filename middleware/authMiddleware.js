22072cf713ba705040b808c50acc0261b0e6a9d8
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2025-08-24 23:16:10 -0400

    Configured authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,6 +11,12 @@ const authMiddleware = (req, res, next) => {
 
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		// Ensure token has a valid payload
+		if (!decoded.id) {
+			return res.status(401).json({ message: "Invalid token payload" });
+		}
+
 		req.user = decoded;
 		next();
 	} catch (err) {
