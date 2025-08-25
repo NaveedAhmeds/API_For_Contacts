7f5dc02b17eda6d61d66784835fce9c4410dc248
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2025-08-24 23:09:15 -0400

    Configured User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
 	{
@@ -10,5 +11,23 @@ const userSchema = new mongoose.Schema(
 	},
 	{ timestamps: true }
 );
+
+// Hash password before saving...
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+
+	try {
+		const salt = await bcrypt.genSalt(10);
+		this.password = await bcrypt.hash(this.password, salt);
+		next();
+	} catch (error) {
+		next(error);
+	}
+});
+
+// Method to compare entered password with hashed password...
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model("User", userSchema);
